1c16141005e60cfae023186c1df2121f1fcc481f
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-12-05 08:19:19 +0600

    capture macos full screen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use screenshots::Screen;
-use std::{path::PathBuf, time::Instant};
+use anyhow::Result;
+use core_graphics::display::CGDisplay;
+use core_graphics::image::CGImage;
+use image::{ImageBuffer, Rgb};
+use std::path::PathBuf;
 
-fn main() {
-    let root = PathBuf::new().join("ScreenShots");
+#[cfg(target_os = "linux")]
+fn linux(root: PathBuf) -> Result<()> {
     let start = Instant::now();
-    let screens = Screen::all().unwrap();
+    let screens = Screen::all()?;
 
     for screen in screens {
         println!("capturer {:?}", screen);
@@ -30,4 +33,54 @@ fn main() {
     image.save(path.clone()).unwrap();
 
     println!("Total Elapsed Time : {:?}", start.elapsed());
+}
+
+#[cfg(target_os = "macos")]
+fn macos(root: PathBuf) -> Result<()> {
+    use anyhow::bail;
+
+    let display = CGDisplay::main();
+
+    // let image: CGImage = display.image()?;
+    match display.image() {
+        Some(image) => {
+            let width = image.width() as u32;
+            let height = image.height() as u32;
+
+            let data = image.data();
+            let bytes_per_row = image.bytes_per_row();
+
+            let mut buffer = ImageBuffer::new(width, height);
+
+            for y in 0..height {
+                for x in 0..width {
+                    let offset = (y as usize * bytes_per_row) + (x as usize * 4);
+                    let pixel = &data[offset..offset + 4];
+                    buffer.put_pixel(x, y, Rgb([pixel[2], pixel[1], pixel[0]]));
+                }
+            }
+            let path = root.join("Full_Screen.png");
+            buffer.save(path.clone())?;
+
+            println!("Screenshot saved to {:?}", path);
+            Ok(())
+        }
+        None => bail!("Failed to init image"),
+    }
+}
+
+fn main() {
+    let root = PathBuf::new().join("ScreenShots");
+
+    // Conditionally call platform-specific functions
+    #[cfg(target_os = "linux")]
+    linux(root);
+
+    #[cfg(target_os = "macos")]
+    if let Err(e) = macos(root) {
+        eprintln!("Error on macOS: {}", e);
+    }
+
+    #[cfg(not(any(target_os = "linux", target_os = "macos")))]
+    println!("This platform is not supported.");
 }
